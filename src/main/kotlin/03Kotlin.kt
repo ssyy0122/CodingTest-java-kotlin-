99fdd4b7bd4b3010408b548c1a8db89ea4d6adbd
ssyy0122 <z01039118740@gmail.com>
2022-10-05 15:58:31 +0900

    조건문

--- src/main/kotlin/03Kotlin.kt
+++ src/main/kotlin/03Kotlin.kt
@@ -1,8 +1,15 @@
 //함수
 fun main() {
-println(add(1,2,3))
+    println(add(1,2,3))
+    println(max(2,5))
 }
 //함수의 기본 fun 함수이름(매개변수: type): 리턴 타입
 fun add(a: Int,b: Int,c:Int):Int {
     return a + b + c;
+}
+//결과를 반환하는 함수
+fun max(a: Int,b:Int): Int{
+        return if (a>b) a else b
+//더 간결한 식
+//fun max(a: Int, b: Int): Int = if (a > b) a else b
 }
---
+++ src/main/kotlin/04Kotlin.kt
@@ -0,0 +1,9 @@
+//조건문
+fun main(){
+    var a =7
+    if (a>6){
+        println(a)
+    }else{
+        println("exit")
+    }
+}
