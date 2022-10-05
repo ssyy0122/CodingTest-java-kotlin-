9adc6225fe63d3de8afff9b9dab24aa5915c97b7
ssyy0122 <z01039118740@gmail.com>
2022-10-05 20:22:12 +0900

    문자열 템플릿

--- src/main/kotlin/04Kotlin.kt
+++ src/main/kotlin/04Kotlin.kt
@@ -1,3 +1,5 @@
+import java.util.Arrays
+
 //조건문
 fun main(){
     var a =7
@@ -6,4 +8,10 @@ fun main(){
     }else{
         println("exit")
     }
+}
+//문자열 템플릿
+fun main2(args: Array<String>){
+    val name = if (args.size>0) args[0]else "kotlin"
+    println("hello,$name")
+
 }
